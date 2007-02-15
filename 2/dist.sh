08969077fa8e0ae6e4480dc1d4441dbbce6df873
babali <babali@f05da9f4-cf23-0410-b09c-be0072a6ac68>
2007-02-15 20:10:34 +0000

    dist.sh now create the tarball with the folder mononoke-notify-2

--- 2/dist.sh
+++ 2/dist.sh
@@ -2,11 +2,12 @@
 
 DISTFILES="AUTHORS CMakeLists.txt COPYING Doxyfile README"
 DISTFILES="$DISTFILES mononoke-notify-2.pro src themes"
-DISTDIR=mononoke-notify-2-`date +%Y%m%d`
+DISTDIR=mononoke-notify-2
+VERSION=`date +%Y%m%d`
 
-rm -rf $DISTDIR $DISTDIR.tar.bz2
+rm -rf $DISTDIR ${DISTDIR}_$VERSION.tar.bz2
 mkdir $DISTDIR
 cp -R $DISTFILES $DISTDIR/
 find $DISTDIR/ -name '.svn' | xargs rm -rf
-tar -cvjf $DISTDIR.tar.bz2 $DISTDIR
+tar -cvjf ${DISTDIR}_$VERSION.tar.bz2 $DISTDIR
 rm -rf $DISTDIR
